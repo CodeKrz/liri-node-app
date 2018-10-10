2092167ab992733a0a71151d5ff6ffad0c33dfa9
Jose <krzduaz@yahoo.com>
2018-10-09 18:20:12 -0700

    2nd commit

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'AwjAXDZ28e8eGPvVHl6dHE51u',
+  consumer_secret: 'dbpifIyGR6viQ5cCDOyyKlPDDwoxmZvMbhH4LTuXLsrWvqieWb',
+  access_token_key: '3885326479-pYKYP7NfARIPh8fcseYNN8jsvjcW7a5kB0u7oiV',
+  access_token_secret: 'BimsVaruUP3k4JgvYcYN9xtqOVQDuQ1xKxKFfi3N2vf2t'
+}
+
+
+
+
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
